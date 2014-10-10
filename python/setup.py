ee14152927b8cfab27cedb8e23e53546f2a784ff
sloev <johannesgj@gmail.com>
2014-10-10 23:10:09 +0200

    fixed setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,9 +2,10 @@ from distutils.core import setup
 
 setup(
         name="jsonplustypes",
-        version="0.3",
+        version="0.4",
         packages=["jsonplustypes",],
         license="MIT",
+        description="json with datetime and more, KISS",
         long_description=open("README.txt").read(),
         maintainer="sloev",
         maintainer_email="johannesgj@gmail.com",
